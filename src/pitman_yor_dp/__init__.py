acd58357465078d06dfe1489eb277dfc2cb15884
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-07 02:06:36 +0200

    Export diagnostics and clutter helpers

--- src/pitman_yor_dp/__init__.py
+++ src/pitman_yor_dp/__init__.py
@@ -7,11 +7,43 @@ from .cardinality import (
     validate_cluster_sizes,
     validate_nonnegative_int,
 )
+from .clutter import (
+    NoveltyClutterOdds,
+    new_target_vs_clutter_odds,
+    novelty_prior_probability,
+    phase_transition_table,
+)
+from .diagnostics import (
+    CountSeriesDiagnostics,
+    LogLogSurvivalFit,
+    TailIndexEstimate,
+    birth_counts_from_label_sets,
+    count_series_diagnostics,
+    hill_tail_index,
+    lifetimes_from_label_sets,
+    live_counts_from_label_sets,
+    loglog_survival_fit,
+    survival_function,
+)
 
 __all__ = [
+    "CountSeriesDiagnostics",
     "DirichletProcessCardinalityPrior",
+    "LogLogSurvivalFit",
+    "NoveltyClutterOdds",
     "PitmanYorCardinalityPrior",
+    "TailIndexEstimate",
+    "birth_counts_from_label_sets",
+    "count_series_diagnostics",
     "empirical_pmf",
+    "hill_tail_index",
+    "lifetimes_from_label_sets",
+    "live_counts_from_label_sets",
+    "loglog_survival_fit",
+    "new_target_vs_clutter_odds",
+    "novelty_prior_probability",
+    "phase_transition_table",
+    "survival_function",
     "validate_cluster_sizes",
     "validate_nonnegative_int",
 ]
